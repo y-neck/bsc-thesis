8a07764659c80518594fa527bc2c7ac0580fb013
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-04-11 12:00:21 +0200

    Initial commit (crawl_keystone-sda)

---
+++ crawler/bsc_crawl_keystone-sda.py
@@ -0,0 +1,6 @@
+# Installed packages: requests, beautifulsoup4, seleniumbase
+# © 2025 https://github.com/y-neck
+#!! This script is designed for scientific use only and should not be used for other purposes.
+
+#base url to be crawled
+
